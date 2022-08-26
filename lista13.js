414cf11eceaf8f46d507f679fff7da7753160f32
Alexandre Lipari <santuslipari@gmail.com>
2022-08-26 19:17:40 -0300

    Inserir login e percorrer o array para encontrar, caso for falso exibir mensagem

--- lista13.js
+++ lista13.js
@@ -2,6 +2,9 @@ var nomes = []
 var senhas = []
 var contador = 0
 var opcao = 0
+var nomeLogin
+var senhaLogin
+var loginValido = false
 var continuar = 1, continuarTotal = true
 while (continuarTotal == 1) {
     opcao = prompt("O que deseja? (1) Cadastrar (2) Login (3) Finalizar")
@@ -13,10 +16,22 @@ while (continuarTotal == 1) {
             continuar = parseInt(prompt("Deseja continuar? (1) SIM (2) NÃO"))
         }
         continuar = 1
-    }else if (opcao == 2){
+    } else if (opcao == 2) {
+        nomeLogin = promp("nome login:")
+        senhaLogin = prompt("senha login")
+        loginValido = false
+        for (var i = 0; i < contador; i++) {
+            if (nomeLogin == nomes[i] && senhaLogin == senhas[i]) {
+                loginValido = true
+                console.log("login realizado com sucesso!")
+            }
+        }
+        if (!loginValido) {
+            console.log("nome ou senha incorretos")
+        }
         console.log("Login feito")
-    }else{
-continuarTotal = false
+    } else {
+        continuarTotal = false
     }
 }
 
