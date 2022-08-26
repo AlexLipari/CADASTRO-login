6a41ffd9baef74311247ee1d8ca0000c566c13c5
Alexandre Lipari <santuslipari@gmail.com>
2022-08-26 18:04:21 -0300

    Opçã

--- lista13.js
+++ lista13.js
@@ -1,14 +1,25 @@
 var nomes = []
 var senhas = []
 var contador = 0
-var continuar =1
-while (continuar == 1) {
-    nomes[contador] = prompt("insira um nome:")
-    senhas[contador] = prompt("insira uma senha:")
-    contador++
-    continuar = parseInt(prompt("Deseja continuar? (1) SIM (2) NÃO"))
+var opcao = 0
+var continuar = 1, continuarTotal = true
+while (continuarTotal == 1) {
+    opcao = prompt("O que deseja? (1) Cadastrar (2) Login (3) Finalizar")
+    if (opcao == 1) {
+        while (continuar == 1) {
+            nomes[contador] = prompt("insira um nome:")
+            senhas[contador] = prompt("insira uma senha:")
+            contador++
+            continuar = parseInt(prompt("Deseja continuar? (1) SIM (2) NÃO"))
+        }
+        continuar = 1
+    }else if (opcao == 2){
+        console.log("Login feito")
+    }else{
+continuarTotal = false
+    }
 }
-continuar=1
+
 console.log(nomes)
 console.log(senhas)
 
